cf5e52f6bab2222b96efc133d59474e32c56a8f0
Liuda <liudaprysiazhna@gmail.com>
2018-11-25 00:56:43 +0200

    'migrations'

--- myproject/myproject/boards/models.py
+++ myproject/myproject/boards/models.py
@@ -13,15 +13,18 @@ class Board(models.Model):
 class Topic(models.Model):
     subject = models.CharField(max_length=255)
     last_updated = models.DateTimeField(auto_now_add=True)
-    board = models.ForeignKey(Board, related_name='topics')
-    starter = models.ForeignKey(User, related_name='topics')
+    board = models.ForeignKey(Board, related_name='topics', on_delete = models.DO_NOTHING)
+    starter = models.ForeignKey(User, related_name='topics', on_delete = models.DO_NOTHING)
+
+
+
 
 
 class Post(models.Model):
     message = models.TextField(max_length=4000)
-    topic = models.ForeignKey(Topic, related_name='posts')
+    topic = models.ForeignKey(Topic, related_name='posts', on_delete = models.DO_NOTHING)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(null=True)
-    created_by = models.ForeignKey(User, related_name='posts')
-    updated_by = models.ForeignKey(User, null=True, related_name='+')
+    created_by = models.ForeignKey(User, related_name='posts', on_delete = models.DO_NOTHING)
+    updated_by = models.ForeignKey(User, null=True, related_name='+', on_delete = models.DO_NOTHING)
 
