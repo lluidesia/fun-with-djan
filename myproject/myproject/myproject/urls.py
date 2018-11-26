ac38f0e87f61c037baa0667bd79d775c38eb89ce
Liuda <liudaprysiazhna@gmail.com>
2018-11-26 21:10:50 +0200

    Urls and board pages

--- myproject/myproject/myproject/urls.py
+++ myproject/myproject/myproject/urls.py
@@ -13,13 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-# from django.urls import path
 from django.conf.urls import url
+from django.contrib import admin
+
 from boards import views
 
 
 urlpatterns = [
-    url('admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
+    url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    url(r'^admin/', admin.site.urls),
 ]
