d5a6864820c3197547f7a671e2a15e8a80b5b1ff
Liuda <liudaprysiazhna@gmail.com>
2018-12-14 00:47:47 +0200

    Split tests from one file to few ones and save in new folder tests

--- myproject/myproject/accounts/tests/test_view_signup.py
+++ myproject/myproject/accounts/tests/test_view_signup.py
@@ -1,11 +1,9 @@
-# from django.core.urlresolvers import reverse
 from django.urls import reverse
-from django.urls import resolve
-#from django.contrib.auth.forms import UserCreationForm
-from .forms import SignUpForm
 from django.contrib.auth.models import User
+from django.urls import resolve
 from django.test import TestCase
-from .views import signup
+from ..views import signup
+from ..forms import SignUpForm
 
 
 class SignUpTests(TestCase):
