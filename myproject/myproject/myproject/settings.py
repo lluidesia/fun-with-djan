d8a7011d926657ada31e1ff8083d26477ab59d62
Liuda <liudaprysiazhna@gmail.com>
2018-12-15 20:54:36 +0200

    Added logout

--- myproject/myproject/myproject/settings.py
+++ myproject/myproject/myproject/settings.py
@@ -121,8 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGOUT_REDIRECT_URL = 'home'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
 
